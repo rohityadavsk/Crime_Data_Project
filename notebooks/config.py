c7a9a62598efeb363a0fad013729ff28e2dd1809
Rohit Yadav <rohityadavsk@gmail.com>
2025-03-02 10:01:19 +0530

    updated config

--- notebooks/config.py
+++ notebooks/config.py
@@ -11,9 +11,6 @@ def create_spark_session():
     """Initialise the spark session"""
     spark = SparkSession.builder \
         .appName("Crime Data Pipeline") \
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config("spark.jars.packages", "io.delta:delta-core_2.12:3.0.0") \
         .config("spark.sql.shuffle.partitions", "2") \
         .config("spark.sql.debug.maxToStringFields", "100") \
         .config("spark.driver.memory", "2g") \
