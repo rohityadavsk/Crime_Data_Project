0120d1716b4d95931e0bab56ab51d10aad1ea347
Rohit Yadav <rohityadavsk@gmail.com>
2025-03-02 10:08:28 +0530

    depend

--- notebooks/config.py
+++ notebooks/config.py
@@ -12,7 +12,6 @@ def create_spark_session():
     spark = SparkSession.builder \
         .appName("Crime Data Pipeline") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
         .config("spark.sql.shuffle.partitions", "2") \
         .config("spark.sql.debug.maxToStringFields", "100") \
         .config("spark.driver.memory", "2g") \
