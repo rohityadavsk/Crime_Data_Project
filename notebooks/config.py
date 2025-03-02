24cb1354a92c8d86d05ed6aefb79b75d6c1f6430
Rohit Yadav <rohityadavsk@gmail.com>
2025-03-02 10:19:08 +0530

    depe

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 import os
+import delta
 
 raw_data_path = os.environ.get("raw_data_path")
 target_path = os.environ.get("target_path")
@@ -7,17 +8,32 @@ target_path = os.environ.get("target_path")
 # Environment settings
 os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
 
+# def create_spark_session():
+#     """Initialise the spark session"""
+#     spark = SparkSession.builder \
+#         .appName("Crime Data Pipeline") \
+#         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+#         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+#         .config("spark.sql.shuffle.partitions", "2") \
+#         .config("spark.sql.debug.maxToStringFields", "100") \
+#         .config("spark.driver.memory", "2g") \
+#         .config("spark.executor.memory", "2g") \
+#         .config("spark.default.parallelism", "4") \
+#         .getOrCreate()
+#
+#     return spark
+
+
 def create_spark_session():
-    """Initialise the spark session"""
-    spark = SparkSession.builder \
-        .appName("Crime Data Pipeline") \
+    """Initialize the Spark session with Delta Lake support"""
+    builder = SparkSession.builder.appName("Crime Data Pipeline") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
         .config("spark.sql.shuffle.partitions", "2") \
         .config("spark.sql.debug.maxToStringFields", "100") \
         .config("spark.driver.memory", "2g") \
         .config("spark.executor.memory", "2g") \
-        .config("spark.default.parallelism", "4") \
-        .getOrCreate()
+        .config("spark.default.parallelism", "4")
 
+    spark = delta.configure_spark_with_delta_pip(builder).getOrCreate()
     return spark
