97e670d917e476f13f295a6f68dcb2523c3d389a
Rohit Yadav <rohityadavsk@gmail.com>
2025-03-02 09:54:18 +0530

    Add delta version

--- notebooks/config.py
+++ notebooks/config.py
@@ -13,7 +13,7 @@ def create_spark_session():
         .appName("Crime Data Pipeline") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config("spark.jars.packages", "io.delta:delta-core_2.12:3.3.0") \
+        .config("spark.jars.packages", "io.delta:delta-core_2.12:3.0.0") \
         .config("spark.sql.shuffle.partitions", "2") \
         .config("spark.sql.debug.maxToStringFields", "100") \
         .config("spark.driver.memory", "2g") \
